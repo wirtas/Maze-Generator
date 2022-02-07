0c04d00b120c97d422e60c9c1c3f8ead35e0e813
Wirtas <wirtekkr@gmail.com>
2022-02-07 09:25:18 +0100

    Added background tiles

--- Assets/Scripts/MazeRenderer.cs
+++ Assets/Scripts/MazeRenderer.cs
@@ -2,25 +2,22 @@ using UnityEngine;
 
 public class MazeRenderer : MonoBehaviour
 {
-    [SerializeField] [Range(5,100)] 
+    [SerializeField] [Range(5,30)] 
     private int width = 10;
     
-    [SerializeField] [Range(5,100)] 
+    [SerializeField] [Range(5,30)] 
     private int height = 10;
     
-    [SerializeField] [Range(0.5f,5)] 
+    [SerializeField] [Range(0.5f,3)] 
     private float size = 1;
     
     [SerializeField] 
-    private Transform wallPrefab;
-
-    [SerializeField] private Transform bg;
+    private Transform wallPrefab, startPrefab, endPrefab, background, bgParent;
 
     private void Start()
     {
         var maze = MazeGenerator.Generate(width, height);
         Draw(maze);
-        SetBackground();
     }
 
     private void Draw(WallState[,] maze)
@@ -31,24 +28,24 @@ public class MazeRenderer : MonoBehaviour
             {
                 WallState cell = maze[i, j];
 
-                Vector2 position = new Vector2(-width / 2 + i + 0.5f,  -width / 2 + j + 0.5f)*size;
-                
+                Vector3 position = new Vector3(-width / 2 + i, -width / 2 + j) * size;
+
                 if (cell.HasFlag(WallState.Up))
                 {
                     Transform upWall = Instantiate(wallPrefab, transform);
-                    upWall.position = position + new Vector2(0, size / 2);
-                    Vector2 upWallLocalScale = upWall.localScale;
-                    upWallLocalScale = new Vector2(size, upWallLocalScale.y);
+                    upWall.position = position + new Vector3(0, size / 2);
+                    Vector3 upWallLocalScale = upWall.localScale;
+                    upWallLocalScale = new Vector3(size, upWallLocalScale.y);
                     upWall.localScale = upWallLocalScale;
                 }
 
                 if (cell.HasFlag(WallState.Left))
                 {
                     Transform leftWall = Instantiate(wallPrefab, transform);
-                    leftWall.position = position + new Vector2(-size / 2, 0);
-                    Vector2 leftWallLocalScale = leftWall.localScale;
+                    leftWall.position = position + new Vector3(-size / 2, 0);
+                    Vector3 leftWallLocalScale = leftWall.localScale;
                     leftWall.eulerAngles = new Vector3(0, 0, 90);
-                    leftWallLocalScale = new Vector2(size, leftWallLocalScale.y);
+                    leftWallLocalScale = new Vector3(size, leftWallLocalScale.y);
                     leftWall.localScale = leftWallLocalScale;
                 }
 
@@ -57,12 +54,12 @@ public class MazeRenderer : MonoBehaviour
                     if (cell.HasFlag(WallState.Right))
                     {
                         Transform rightWall = Instantiate(wallPrefab, transform);
-                        rightWall.position = position + new Vector2(size / 2, 0);
-                        Vector2 rightWallLocalScale = rightWall.localScale;
+                        rightWall.position = position + new Vector3(size / 2, 0);
+                        Vector3 rightWallLocalScale = rightWall.localScale;
                         rightWall.eulerAngles = new Vector3(0, 0, 90);
-                        rightWallLocalScale = new Vector2(size, rightWallLocalScale.y);
+                        rightWallLocalScale = new Vector3(size, rightWallLocalScale.y);
                         rightWall.localScale = rightWallLocalScale;
-                    } 
+                    }
                 }
 
                 if (j == 0)
@@ -70,19 +67,38 @@ public class MazeRenderer : MonoBehaviour
                     if (cell.HasFlag(WallState.Down))
                     {
                         Transform downWall = Instantiate(wallPrefab, transform);
-                        downWall.position = position + new Vector2(0, -size / 2);
-                        Vector2 downWallLocalScale = downWall.localScale;
-                        downWallLocalScale = new Vector2(size, downWallLocalScale.y);
+                        downWall.position = position + new Vector3(0, -size / 2);
+                        Vector3 downWallLocalScale = downWall.localScale;
+                        downWallLocalScale = new Vector3(size, downWallLocalScale.y);
                         downWall.localScale = downWallLocalScale;
                     }
                 }
+
+                if (i == 0 && j == 0)
+                {
+                    Transform startPosition = Instantiate(startPrefab, bgParent);
+                    startPosition.position = position+ new Vector3(0,0,1);
+                    Vector3 startPositionLocalScale = startPosition.localScale;
+                    startPositionLocalScale = new Vector3(size, startPositionLocalScale.y);
+                    startPosition.localScale = startPositionLocalScale;
+                }
+                else if (i == width - 1 && j == height - 1)
+                {
+                    Transform endPosition = Instantiate(endPrefab, bgParent);
+                    endPosition.position = position+ new Vector3(0,0,1);
+                    Vector3 endPositionLocalScale = endPosition.localScale;
+                    endPositionLocalScale = new Vector3(size, endPositionLocalScale.y);
+                    endPosition.localScale = endPositionLocalScale;
+                }
+                else
+                {
+                    Transform bg = Instantiate(background, bgParent);
+                    bg.position = position + new Vector3(0,0,1);
+                    Vector3 bgLocalScale = bg.localScale;
+                    bgLocalScale = new Vector3(size, bgLocalScale.y);
+                    bg.localScale = bgLocalScale;
+                }
             }
         }
-    }
-
-    private void SetBackground()
-    {
-        bg.localScale = new Vector3(width, height, bg.localScale.z)*size;
-        bg.gameObject.SetActive(true);
     }
 }
